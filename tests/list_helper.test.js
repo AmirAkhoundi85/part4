3d6ffc1ad0cbe8e30f839d8a59f5a3814a36d345
Amir <amir.akhoundi85@gmail.com>
2024-01-31 13:36:29 +0200

    completed 4-4

---
+++ tests/list_helper.test.js
@@ -0,0 +1,65 @@
+const listHelper = require("../utils/list_helper");
+
+test("dummy returns one", () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
+
+describe("total likes", () => {
+  const emptyListBlogs = [];
+  test("empty list is zero", () => {
+    const result = listHelper.totalLikes(emptyListBlogs);
+    expect(result).toBe(0);
+  });
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    }
+  ];
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  
+
+    const manyListBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f7",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 4,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f9",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 6,
+        __v: 0,
+      },
+    ];
+    test("of a bigger list is claculated right", () => {
+      const result = listHelper.totalLikes(manyListBlog);
+      expect(result).toBe(15);
+    });
+});
