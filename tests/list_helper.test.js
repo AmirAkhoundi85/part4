c7ec6d40735120cac8b2825247bbf31ad5584f8e
Amir <amir.akhoundi85@gmail.com>
2024-02-02 11:48:43 +0200

    completed 4-5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -63,3 +63,59 @@ describe("total likes", () => {
       expect(result).toBe(15);
     });
 });
+
+
+describe("the most likes", ()=>{
+  const blogs = [];
+  test("when epmty blogs list", ()=>{
+    const result = listHelper.favoriteBlog(blogs);
+    expect(result).toEqual(null)
+  })
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  test("when list has only one blog", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toStrictEqual(listWithOneBlog[0]);
+
+  });
+  const manyListBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f7",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 4,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 6,
+      __v: 0,
+    },
+  ];
+  test("of a bigger list ,return the blog with highest number of likes", () => {
+    const result = listHelper.favoriteBlog(manyListBlog);
+    expect(result).toStrictEqual(manyListBlog[2]);
+  });
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,16 +1,28 @@
 const dummy = (blogs) => {
-  return 1
+  return 1;
 };
 
-const totalLikes = (blogs) =>{
-    let sum=0;
-    blogs.forEach(item => {
-        sum += item.likes
-    });
-    return sum;
-}
+const totalLikes = (blogs) => {
+  let sum = 0;
+  blogs.forEach((item) => {
+    sum += item.likes;
+  });
+  return sum;
+};
+
+const favoriteBlog = (blogs) => {
+  if (!blogs || !blogs.length) {
+    return null;
+  }
+  const likesArray = blogs.map((item) => item.likes); // [3, 5, 10]
+  let max = Math.max(...likesArray);
+  let result = blogs.find((blog) => blog.likes === max); //
+
+  return result;
+};
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 };
