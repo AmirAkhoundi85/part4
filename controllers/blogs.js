06cde7771bd21dde83387fe4554eb414b8e6ffcb
Amir <amir.akhoundi85@gmail.com>
2024-02-09 12:43:43 +0200

    completed 4-13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,18 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-
+require("express-async-errors");
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.get("/:id", (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (blog) {
-        response.json(blog);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
 });
 
 blogsRouter.post("/", async (request, response, next) => {
@@ -28,22 +25,13 @@ blogsRouter.post("/", async (request, response, next) => {
     likes,
   });
 
-  try {
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (exception) {
-    next(exception);
-  }
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
-
-
-blogsRouter.delete("/:id", (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 blogsRouter.put("/:id", (request, response, next) => {
