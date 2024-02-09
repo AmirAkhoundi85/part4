b514c8b21bda39e10273620af5f4a8eb06676920
Amir <amir.akhoundi85@gmail.com>
2024-02-09 12:16:20 +0200

    completed 4-9

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -5,34 +5,30 @@ const app = require("../app");
 const api = supertest(app);
 
 const Blog = require("../models/blog");
+const helper = require("./test_helper");
 
-const initialBlogs = [
-  {
-    title: "Html",
-    author: "Amir",
-    url: "",
-    likes: 10,
-  },
-  {
-    title: "CSS",
-    author: "Amir",
-    url: "",
-    likes: 20,
-  },
-];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(initialBlogs[0]);
-  await blogObject.save();
-  blogObject = new Blog(initialBlogs[1]);
-  await blogObject.save();
+  helper.initialBlogs.forEach(async (blog) => {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+    console.log("saved");
+  });
 });
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
-
-  expect(response.body).toHaveLength(initialBlogs.length);
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
+
+test("verifies that the unique identifier property of the blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
+  response.body.forEach((item) => {
+    expect(item.id).toBeDefined();
+  });
+
+});
+
 
 afterAll(async () => {
   await mongoose.connection.close();
